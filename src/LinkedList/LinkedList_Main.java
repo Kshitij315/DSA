b9462fc770aececf0da0cde8502d89e853fc420c
kshitij khandkar <kshitijkk15@gmail.com>
2024-06-11 16:12:05 +0530

    Linked list addded

---
+++ src/LinkedList/LinkedList_Main.java
@@ -0,0 +1,50 @@
+package LinkedList;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinkedList_Main {
+    public static void main(String args[])
+    {
+        int ch,e;
+        Scanner in = new Scanner(System.in);
+        Linked_List obj = new Linked_List();
+        obj.create_list();//user given size :list
+        do {
+            System.out.println("\n1.Insert Left\n2.Insert Right\n3.Delete Left\n4.Delete Right\n5.Print List\n6.Search\n0.Exit\n:");
+            ch = in.nextInt();
+            switch (ch) {
+                case 1:
+                    System.out.println("Enter data:");
+                    e = in.nextInt();
+                    obj.insert_left(e);
+                    break;
+                case 2:
+                    System.out.println("Enter data:");
+                    e = in.nextInt();
+                    obj.insert_right(e);
+                    break;
+                case 3:
+                    obj.delete_left();
+                    break;
+                case 4:
+                    obj.delete_right();
+                    break;
+                case 5:
+                    obj.print_list();
+                    break;
+                case 6:
+                    System.out.println("Enter data:");
+                    e = in.nextInt();
+                    obj.search_list(e);
+                    break;
+                case 0:
+                    System.out.println("Exiting.....");
+                    break;
+                default:
+                    System.out.println("Wrong option selected");
+                    break;
+            }
+        } while (ch != 0);
+    }
+}
