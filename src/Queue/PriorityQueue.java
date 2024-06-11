fcdd71f63cebde69bac13ad62d412cb119d74f0e
kshitij khandkar <kshitijkk15@gmail.com>
2024-06-11 16:04:50 +0530

    Linked list added

--- src/Queue/PriorityQueue.java
+++ src/Queue/PriorityQueue.java
@@ -1,2 +1,56 @@
-package Queue;public class PriorityQueue {
+package Queue;
+
+public class PriorityQueue {
+    int q[],maxsize,front,rear;
+    void create_queue(int size)
+    {
+        maxsize=size;
+        front=0;
+        rear=-1;
+        q=new int[maxsize];
+    }
+    void enqueue(int e)
+    {
+        //q[++rear]=e;
+        rear++;
+        q[rear]=e;
+        for(int i=front;i<rear;i++) {
+            for (int j = front; j < rear; j++) {
+                if(q[j]>q[j+1])
+                {
+                    int temp=q[j];
+                    q[j]=q[j+1];
+                    q[j+1]=temp;
+                }
+
+            }
+        }
+    }
+
+    boolean is_full()
+    {
+        if(rear==maxsize-1)
+            return true;
+        else
+            return false;
+    }
+
+    int dequeue()
+    {
+        int temp=q[front];
+        front++;
+        return temp;
+    }
+    boolean is_empty()
+    {
+        if(front>rear)
+            return true;
+        else
+            return false;
+    }
+    void print_queue()
+    {
+        for(int i=front;i<=rear;i++)
+            System.out.print(q[i]+"--");
+    }
 }
